5d9facce78592796028896959f60b569cf09ef2e
ONavarroGeraldino <oswaldonavarro0308@gmail.com>
2026-07-24 22:43:20 -0400

    fix: force unregister all old service workers on page load to clear stale cache

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(regs => {
+    regs.forEach(r => r.unregister());
+  });
+}
+
 createRoot(document.getElementById('root')).render(
   <App />
 )
